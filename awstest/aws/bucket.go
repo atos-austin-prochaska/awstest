879af5b0f6325170ee64a7d85b3c4f5694208899
atos-austin-prochaska <94704917+atos-austin-prochaska@users.noreply.github.com>
2021-12-01 15:00:56 -0600

    The start of something

---
+++ awstest/aws/bucket.go
@@ -0,0 +1,44 @@
+package aws
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type S3ListBucketsAPI interface {
+	ListBuckets(ctx context.Context,
+		params *s3.ListBucketsInput,
+		optFns ...func(*s3.Options)) (*s3.ListBucketsOutput, error)
+}
+
+func GetAllBuckets(c context.Context, api S3ListBucketsAPI, input *s3.ListBucketsInput) (*s3.ListBucketsOutput, error) {
+	return api.ListBuckets(c, input)
+}
+
+func ShowBuckets() {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("configuration error, " + err.Error())
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	input := &s3.ListBucketsInput{}
+
+	result, err := GetAllBuckets(context.TODO(), client, input)
+	if err != nil {
+		fmt.Println("Got an error retrieving buckets:")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Buckets:")
+
+	for _, bucket := range result.Buckets {
+		fmt.Println(*bucket.Name + ": " + bucket.CreationDate.Format("2006-01-02 15:04:05 Monday"))
+	}
+	fmt.Println("\n" + *result.Owner.DisplayName + ": " + *result.Owner.ID)
+}
---
+++ awstest/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ShowBuckets()
+	fmt.Println("test")
+
+}
