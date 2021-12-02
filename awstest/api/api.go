038c98734d56df04c6061212715f6af8c1491b3d
atos-austin-prochaska <94704917+atos-austin-prochaska@users.noreply.github.com>
2021-12-02 13:34:02 -0600

    added routes

--- awstest/api/api.go
+++ awstest/api/api.go
@@ -8,7 +8,19 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("test"))
+	w.Write([]byte("testing"))
+}
+
+func accessKeys(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("access keys go here"))
+}
+
+func buckets(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("buckets go here"))
+}
+
+func objects(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("objects go here"))
 }
 
 func Run() {
@@ -21,6 +33,9 @@ func Run() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/ack", accessKeys)
+	mux.HandleFunc("/buk", buckets)
+	mux.HandleFunc("/obj", objects)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
